0c3fbd430fd6283e6cc630cada2318253a947148
nikhilmishra1710 <nikhilmishra1710@gmail.com>
2025-01-22 23:43:58 +0530

    Added basic exit command support

--- main.c
+++ main.c
@@ -3,6 +3,19 @@
 
 typedef enum { false, true } bool;
 
+bool compareString(char *str1, char *str2)
+{
+  if (strlen(str1) != strlen(str2))
+  {
+    return false;
+  }
+  else if(strcmp(str1, str2) != 0)
+  {
+    return false;
+  }
+  return true;
+}
+
 int main()
 {
   // Flush after every printf
@@ -22,8 +35,14 @@ int main()
     // Remove trailing newline character
     input[strcspn(input, "\n")] = 0;
 
+    if(compareString(input, "exit 0"))
+    {
+      run = false;
+    }else{
+      printf("%s: command not found\n", input);
+    }
     // Print the user input
-    printf("%s: command not found\n", input);
   }
+
   return 0;
 }
